4bac084d63a8f8599dd5f112cc9391e6bbe409e8
yilingaini98@vip.qq.com <yilingaini98@vip.qq.com>
2019-03-27 21:33:02 +0800

    edit

--- src/mock.js
+++ src/mock.js
@@ -4,7 +4,7 @@ const Mock = require('mockjs');
 //使用mockjs模拟数据
 Mock.mock('/api/data', (req, res) => {
     return Mock.mock({
-        'data|10-31':[{
+        'data|10-32':[{
             date: '@natural',
             name: '@name',
             address: '@county(true)',

ac5aaa892247b185269a1a494835b86c50f9cdac
yilingaini98@vip.qq.com <yilingaini98@vip.qq.com>
2019-03-28 20:55:58 +0800

    11

---
+++ src/components/DialogForm.vue
@@ -0,0 +1,49 @@
+<template>
+    <el-dialog
+            :title="dialogtitle"
+            :visible.sync="dialogisvisible"
+            width="40%"
+            :before-close="handleClose">
+        <span>
+            <alDialogFormContent  :dialogformcontent="fullFields"></alDialogFormContent>
+        </span>
+        <span slot="footer" class="dialog-footer">
+        <el-button @click="handleClose" size="medium">取 消</el-button>
+        <el-button type="primary" @click="handleConfirm"  size="medium">确 定</el-button>
+        </span>
+    </el-dialog>
+</template>
+
+<script>
+    import alDialogFormContent from './DialogFormContent';
+    export default {
+        name: "dialogForm",
+        components:{
+            alDialogFormContent
+        },
+        props:['dialogisvisible','dialogtitle','dialogcontent'],
+        data(){
+            return{
+                dialogVisible:false,
+                fullFields:this.dialogcontent//向父组件获取表头数据
+            }
+        },
+        mounted(){
+
+        },
+        methods:{
+            handleClose() {
+                console.log("我关啦");
+                this.$emit("handleclosedialog", this.dialogVisible);
+            },
+            handleConfirm(){
+                console.log("确认啦")
+                this.$emit("handleclosedialog", this.dialogVisible);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/DialogFormContent.vue
@@ -0,0 +1,48 @@
+<template>
+    <el-form ref="form"
+             label-width="80px"
+
+    >
+        <el-form-item :label=item.label
+                      v-for="item in dialogformcontent"
+
+        >
+            <el-input  v-if="item.type=='string'"
+                       type="input"
+                       size="medium"
+                       v-model=item.value
+            ></el-input>
+            <el-date-picker v-else-if="item.type=='date'"
+                            type="date"
+                            size="medium"
+                            placeholder="选择日期"
+                            v-model=item.value
+                            style="width: 100%;"
+            ></el-date-picker>
+            <el-input v-else-if="item.type=='textarea'"
+                      type="textarea"
+                      size="medium"
+                      v-model=item.value
+            ></el-input>
+        </el-form-item>
+    </el-form>
+</template>
+
+<script>
+    export default {
+        name: "DialogFormContent",
+        props:['dialogformcontent'],
+        data(){
+            return{
+
+            }
+        },
+        mounted() {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
